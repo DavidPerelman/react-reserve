dc27ccbd01e8d7320c248cc29a1c2ef96849ba57
David Perelman <dperelman3@gmail.com>
2023-01-24 23:18:33 +0200

    Fetching Data on the Server with getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -2,23 +2,26 @@ import App from 'next/app';
 import Layout from '../components/_App/Layout';
 import '/static/styles.css';
 import '/static/nprogress.css';
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    console.log(Component);
+    let pageProps = {};
 
-// class MyApp extends App {
-//   render() {
-//     const { Component } = this.props;
-//     return (
-//       <Layout>
-//         <Component />
-//       </Layout>
-//     );
-//   }
-// }
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps: pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
 }
 
-// export default MyApp;
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 
-function Home() {
-  useEffect(() => {
-    getProducts();
-  }, []);
+function Home({ products }) {
+  console.log(products);
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
 
-  const getProducts = async () => {
-    const url = 'http://localhost:3000/api/products';
-    const response = await axios.get(url);
-    console.log(response.data);
-  };
+  // const getProducts = async () => {
+  //   const url = 'http://localhost:3000/api/products';
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  // };
 
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  console.log(response.data);
+
+  return { products: response.data };
+};
 
 export default Home;
